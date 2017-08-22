dbff0644802a5a1d7d05cfda623b5e846bdd5ffd
Chris Greiveldinger <greivel@yahoo.com>
2017-08-21 21:31:51 -0700

    Start on lesson 2.

--- setup/setup_p2.sh
+++ setup/setup_p2.sh
@@ -5,7 +5,7 @@
 # get the correct ami
 export region=$(aws configure get region)
 if [ $region = "us-west-2" ]; then
-   export ami="ami-bc508adc" # Oregon
+   export ami="ami-951609ec" # Oregon
 elif [ $region = "eu-west-1" ]; then
    export ami="ami-b43d1ec7" # Ireland
 elif [ $region = "us-east-1" ]; then
